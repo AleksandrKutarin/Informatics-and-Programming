8689a231d958eb332f4d24d3228a690fb64c6b95
Aleksandr <fames5369@gmail.com>
2021-09-30 20:16:56 +0300

    podbor

---
+++ dzTask2/Podbor.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+#include<locale.h>
+
+void main() 
+{
+	setlocale(LC_ALL, "rus");
+	int a, b;
+	b = rand() % 1000 + 1;
+	do 
+	{
+		printf("¬ведите число: ");
+		scanf_s("%d", &a);
+		if (a > b)
+		{
+			printf("меньше\n");
+		}else if (a < b)
+		{
+			printf("больше\n");
+		}	
+	} while (a != b);
+	printf("WIN");
+	getchar;
+}
