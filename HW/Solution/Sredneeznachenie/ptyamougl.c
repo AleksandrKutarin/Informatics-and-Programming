73f8261c348894ee184e0db913560f56079594d4
Aleksandr <fames5369@gmail.com>
2021-10-19 16:24:29 +0300

    Task0

--- HW/Solution/Sredneeznachenie/ptyamougl.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include <math.h>
-void main() {
-	int a, b;
-	int S;
-	scanf_s("%d %d", &a, &b);
-	S = a * b;
-	printf("%d", S);
-}
--- HW/Solution/Sredneeznachenie/srmodabs.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-void main(){
-	int a, b, c;
-	float x;
-	scanf_s("%d %d %d %f", &a, &b, &c, &x);
-	x = (abs(a) + abs(b) + abs(c)) / 3;
-	printf("%0.1f", x);
-}
